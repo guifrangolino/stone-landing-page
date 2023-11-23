2cc9d37421acea9fe83af0222181af948f7299eb
guifrangolino <gsouza2651@gmail.com>
2023-11-23 20:27:20 -0300

    :sparkles: feat: PageSectionLinks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header";
 import { HeroBanner } from "./components/HeroBanner";
+import { PageSectionLinks } from "./components/PageSectionLinks";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Header />
       <main>
         <HeroBanner />
+        <PageSectionLinks />
       </main>
     </>
   );
