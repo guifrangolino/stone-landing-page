084680e5370c5e40be1759d6d390728991decb47
guifrangolino <gsouza2651@gmail.com>
2023-11-30 18:31:17 -0300

    :lipstick: feat: Section min height

--- src/components/MenosCustoSection.tsx
+++ src/components/MenosCustoSection.tsx
@@ -5,7 +5,7 @@ import menosCustoImgMobile from "../assets/stone-maquininha-de-cartao-menos-cust
 
 export function MenosCustoSection() {
   return (
-    <section className="relative mt-10 flex min-h-[660px] justify-center bg-background4-color md:min-h-[588px] lg:min-h-[558px]">
+    <section className="relative mt-10 flex min-h-[668px] justify-center bg-background4-color md:min-h-[588px] lg:min-h-[558px]">
       <div className="absolute top-[-48px] m-auto flex max-w-7xl flex-col-reverse items-center justify-center px-6 md:top-[-40px] lg:top-[-80px] lg:flex-row">
         <div className="flex max-w-[312px] flex-col items-center justify-center pt-8 md:min-w-[404px] md:max-w-[472px] md:pt-6 lg:mr-4 lg:min-h-[558px] lg:max-w-[404px] lg:items-start lg:pt-[160px]">
           <p className="mb-1 mt-2 w-full text-center font-sharon-display text-sm font-semibold text-color-gray lg:mb-0 lg:text-left lg:text-base ">
